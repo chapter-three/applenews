51ee03c448a83735da3841ea41544145001429cb
Minnur Yunusov <mmyunusov@gmail.com>
2015-08-03 01:19:13 -0700

    Add custom Views handler to display Apple News metadata.

--- js/form.js
+++ js/form.js
@@ -7,12 +7,6 @@
 
   Drupal.behaviors.AppleNewsEntityForm = {
     attach: function(context) {
-      // Display Apple News publish status in vertical tabs.
-      $('fieldset.apple-news-options', context).drupalSetSummary(function (context) {
-        if ($('.form-item-post-to-apple-news-channel input').attr('checked')) {
-          return Drupal.t('Published to Apple News');
-        }
-      });
       $('input[data-channel-id]').each(function() {
         var _channel_id = $(this).data('channel-id');
         if (!$(this).attr('checked')) {
@@ -22,7 +16,7 @@
           $('input[data-section-of="' + _channel_id + '"]').parent().toggle();
         });
       });
-      $('.apple-news-options .sections').parent().css({'margin-left' : '20px'});
+      $('.apple-news-sections').parent().css({'margin-left' : '20px'});
     }
   };
 
