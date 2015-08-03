e7dad807a8efebc84d1bebe41b9dfb65d19139b7
Minnur Yunusov <mmyunusov@gmail.com>
2015-08-02 21:28:19 -0700

    Capture changes.

--- js/form.js
+++ js/form.js
@@ -13,6 +13,16 @@
           return Drupal.t('Published to Apple News');
         }
       });
+      $('input[data-channel-id]').each(function() {
+        var _channel_id = $(this).data('channel-id');
+        if (!$(this).attr('checked')) {
+          $('input[data-section-of="' + _channel_id + '"]').parent().hide();
+        }
+        $(this).click(function() {
+          $('input[data-section-of="' + _channel_id + '"]').parent().toggle();
+        });
+      });
+      $('.apple-news-options .sections').parent().css({'margin-left' : '20px'});
     }
   };
 
